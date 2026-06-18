4791366195bb7f85e195ed43ad40f3f4fa0001a4
Swyam <shahi44swaym@gmail.com>
2026-06-19 03:30:38 +0530

    mouse-event .js

---
+++ mouse-event .js
@@ -0,0 +1,32 @@
+function onClick(){
+    console.log("on click")
+}
+
+
+function onMouseDown(){
+    console.log("on mouse down");
+}
+
+function onMouseUp(){
+    console.log("on mouse up");
+}
+
+function onMouseMove(){
+    console.log("on mouse move");
+}
+
+function ondblclick(){
+    console.log("on double click");
+}
+
+function onMouseOut(){
+    console.log("on mouse out");
+}
+
+function onMouseOver(){
+    console.log("on mouse over");
+}
+
+function onLoad(){
+    alert("Maximum Load");
+}
